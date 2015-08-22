759b380297af5ab5f7db438ee4dd38c003d25d23
hari5hankar <harishankar.vishwanathan@gmail.com>
2015-08-22 14:28:51 -0400

    the symbol table API

---
+++ SymbolTable/SymbolTable.java
@@ -0,0 +1,17 @@
+
+public interface SymbolTable<Key, Value> {
+
+	void put(Key key, Value val);// put key-value pair into the table (remove
+									// key from table if value is null)
+
+	Value get(Key key); // (null if key is absent)
+
+	void delete(Key key); // remove key (and its value) from table
+
+	boolean contains(Key key); // is there a value paired with key?
+
+	boolean isEmpty(); // is the table empty?
+
+	Iterable<Key> keys(); // all the keys in the table
+
+}
